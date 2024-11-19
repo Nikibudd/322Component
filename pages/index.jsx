585742011da3350a218aad464e88e8d60c880b0d
njbro <nj.broennimann@gmail.com>
2024-11-19 19:48:23 +0100

    re-arranged index file

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import TrollTelephoneNumberSelector from "../components/Toll/TrollTelephoneNumberSelector";
+import TrollTelephoneNumberSelector from "../components/Troll/TrollTelephoneNumberSelector";
 import TrollVolume from "../components/TrollVolume";
 import Volume from "../components/Volume";
 
@@ -8,19 +8,15 @@ export default function index() {
 
     
     <>
-            <div className="flex flex-auto">
-            
-            </div>
-    <div className="flex flex-row gap-1">
-        <TrollTelephoneNumberSelector wantedDigits={2} hasPlus={true}/>
-        <TrollTelephoneNumberSelector wantedDigits={3}/>
-        <TrollTelephoneNumberSelector wantedDigits={3}/>
-        <TrollTelephoneNumberSelector wantedDigits={4}/>
-
-    </div>
         <div className="flex flex-auto">
             <TrollVolume/>
             <Volume/>
+            <div className="flex flex-row gap-1">
+                <TrollTelephoneNumberSelector wantedDigits={2} hasPlus={true}/>
+                <TrollTelephoneNumberSelector wantedDigits={3}/>
+                <TrollTelephoneNumberSelector wantedDigits={3}/>
+                <TrollTelephoneNumberSelector wantedDigits={4}/>
+            </div>
         </div>
     </>
     
